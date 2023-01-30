f241810a23cb9353b277ae0092f8090ce19bcfe3
Pierre-Adrien Vasseur <pierreadrien.vasseur@gmail.com>
2023-01-30 21:38:13 +0100

    ✨ Add homepage style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,86 @@
 import Layout from '../components/Layout'
-import { Box, Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import styled from '@emotion/styled'
+import { Button } from '@mui/material'
 
 const Home = () => {
   const navigate = useNavigate()
   return (
     <Layout>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Button variant="contained" onClick={() => navigate('signin')}>
-          SignIn
-        </Button>
-        <Button variant="contained" onClick={() => navigate('signup')}>
-          SignUp
-        </Button>
-      </Box>
+      <Container>
+        <Title>
+          Join the GeoSpot Adventure and <i>discover</i> hidden gems around the
+          world!
+        </Title>
+        <CTA onClick={() => navigate('signup')}>Let&apos;s go!</CTA>
+        <Image src="/earth.png" alt="Earth" />
+      </Container>
     </Layout>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  margin-top: 0.8rem;
+  height: 90vh;
+  padding: 0 0.5rem;
+`
+
+const Title = styled.h1`
+  font-size: 2.4rem;
+  margin: 0.5rem 0;
+  text-align: center;
+
+  i {
+    font-style: bold;
+    color: #f50057;
+  }
+`
+
+const Image = styled.img`
+  position: absolute;
+  width: 125%;
+  bottom: -5%;
+  left: -40%;
+`
+
+const CTA = styled(Button)`
+  background: #f50057;
+  border-radius: 999px;
+  box-shadow: #f50057 0 10px 20px -10px;
+  box-sizing: border-box;
+  color: #ffffff;
+  cursor: pointer;
+  font-family: Inter, Helvetica, 'Apple Color Emoji', 'Segoe UI Emoji',
+    NotoColorEmoji, 'Noto Color Emoji', 'Segoe UI Symbol', 'Android Emoji',
+    EmojiSymbols, -apple-system, system-ui, 'Segoe UI', Roboto, 'Helvetica Neue',
+    'Noto Sans', sans-serif;
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 24px;
+  opacity: 1;
+  outline: 0 solid transparent;
+  padding: 1rem 2rem;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: fit-content;
+  word-break: break-word;
+  border: 0;
+  margin-top: 2rem;
+
+  &:hover {
+    background: #f50057;
+    box-shadow: #f50057 0 10px 20px -10px;
+  }
+
+  &:active {
+    background: #f50057;
+    box-shadow: #f50057 0 10px 20px -10px;
+  }
+`
 
 export default Home
